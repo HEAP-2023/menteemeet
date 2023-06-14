c55524cf1456d18c0c62369873a26cb95c54cda2
Axel Tan <axeltanxl@gmail.com>
2023-06-14 21:29:47 +0800

    Set up server

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config({ path: './.env' });
+
+//Server settings
+const express = require('express');
+const app = express();
+
+//Server params
+const PORT = process.env.PORT;
+const API_VER = process.env.API_VER;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
