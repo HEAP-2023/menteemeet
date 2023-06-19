bf350974d033952668fc6adb8beba2ea174b87dc
alwaystryin88 <srneo.2022@scis.smu.edu.sg>
2023-06-19 20:57:33 +0800

    organiser model update

---
+++ backend/models/organiser.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Organiser = sequelize.define(
+    "Organiser",
+    {
+        ORG_ID: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        DESCRIPTION: {
+            type: DataTypes.String(),
+            allowNull: true
+        }
+    },
+    {
+      timestamps: false,
+      tableName: "organiser"
+    }
+)
+
+module.exports = Organiser
