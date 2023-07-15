422ca462517838c65ebe66922b8ee2b49e49304d
Olivia <oliviaow.2022@scis.smu.edu.sg>
2023-07-15 20:24:47 +0800

    delete events and event filter in Calendar

--- client/src/components/calendar/CalendarFilter.jsx
+++ client/src/components/calendar/CalendarFilter.jsx
@@ -1,23 +1,63 @@
-import { Box, Typography, FormControlLabel, Checkbox} from '@mui/material';
+import { Box, Typography, FormControlLabel, FormControl, Checkbox, FormGroup } from '@mui/material';
+import { useForm, Controller } from "react-hook-form";
+import { useState, useEffect } from 'react';
 
-const CalendarFilter = () => {
+const CalendarFilter = ({ onFiltersChange }) => {
+    const { handleSubmit, control } = useForm();
     const mentorshipProgrammes = fetchMentorshipProgrammes();
+    const [selectedFilters, setSelectedFilters] = useState(() => {
+        const initialFilters = {};
+        mentorshipProgrammes.forEach((item) => {
+            initialFilters[item] = true; //Set all filters to true initially
+        })
+        return initialFilters;
+    })
+
+    const handleSave = (data) => {
+        console.log(data);
+        onFiltersChange(selectedFilters);   
+    }
+
+    useEffect(() => {
+        handleSubmit(handleSave)();
+    }, [selectedFilters]);
+
     return (
-        <Box sx={{ marginTop: '30px', marginX: '16px' }}>
-            <Typography>Sort</Typography>
-            <hr/>
-            {mentorshipProgrammes.map((item, index) => {
-                const name = item;
-                console.log(name);
-                return(
-                    <FormControlLabel
-                    key={index}
-                    control={<Checkbox />}
-                    label={item}
-                    sx={{ display: 'block' }}
-                  />
-                )
-            })}
+        <Box sx={{ marginTop: '57px', marginX: '16px' }}>
+            <Typography>Filter events</Typography>
+            <hr />
+            <FormControl>
+
+                <FormGroup>
+                    {mentorshipProgrammes.map((item, index) => (
+                        <Controller
+                            key={index}
+                            control={control}
+                            name={item}
+                            defaultValue={true}
+                            render={({ field }) => (
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            defaultChecked
+                                            onChange={(e) => {
+                                                field.onChange(e.target.checked); setSelectedFilters((prevFilters) => ({
+                                                    ...prevFilters,
+                                                    [item]: e.target.checked,
+                                                })); 
+                                                handleSubmit(handleSave)();
+                                            }}
+                                            checked={field.checked}
+                                        />
+                                    }
+                                    label={item}
+                                    sx={{ display: 'block' }}
+                                />
+                            )}
+                        />
+                    ))}
+                </FormGroup>
+            </FormControl>
         </Box>
     )
 }
@@ -25,5 +65,5 @@ const CalendarFilter = () => {
 export default CalendarFilter
 
 const fetchMentorshipProgrammes = () => {
-    return ['ABC Programme', 'CDE Programme', 'XYZ Programme']
+    return ['ABC Programme', 'DEF Programme', 'XYZ Programme']
 }
