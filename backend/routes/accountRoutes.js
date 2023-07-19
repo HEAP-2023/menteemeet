dc7ae548c35c1907b618edcee640c86ecf920c07
phosphurous <122954378+phosphurous@users.noreply.github.com>
2023-07-19 19:54:53 +0800

    RegisterDontneedToken

--- backend/routes/accountRoutes.js
+++ backend/routes/accountRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { authenticateToken } = require('../middlewares/authMiddlewares');
 const accountController = require('../controllers/accountController');
 
-router.use(authenticateToken);
-
 router.post('/register', accountController.register);
+
+router.use(authenticateToken);
 router.post('/login', authenticateToken, accountController.login);
 router.post('/logout', authenticateToken, accountController.logout);
 router.post('/changePassword', authenticateToken, accountController.changePassword);
