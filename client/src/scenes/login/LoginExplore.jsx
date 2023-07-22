bb3bb548d7101167737baa3a8cebd02c6c950371
alwaystryin88 <srneo.2022@scis.smu.edu.sg>
2023-07-22 13:03:26 +0800

    login explore updates

--- client/src/scenes/login/LoginExplore.jsx
+++ client/src/scenes/login/LoginExplore.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography, Button} from "@mui/material";
 import { generateColors } from "../../theme";
 import PageHeader from "../../components/PageHeader"
 import Header from "../../components/login/start/Header"
@@ -10,18 +10,32 @@ const loginExplore = () => {
     return (
     <Box width="100%" position="relative">
         <LoginOverlay/>
-        <Box display="grid" gridTemplateRows="1fr 4fr 1fr 4fr" gap="20px" width="100%" p="20px">
+        <Box display="grid" gridTemplateRows="1fr 4fr 1fr 6fr" gap="20px" width="100%" p="20px">
             <Header></Header>
-            <Box sx={{backgroundImage: "url('../images/loginExplore/Untitled design (6).png')"}}
-            borderRadius="20px"
-            paddingTop="180px" paddingLeft="80px" color = "#000000">
+            <Box sx={{backgroundImage: "url('../images/loginExplore/Untitled design (6).png')"}}>
                 {/* <img src="../images/loginExplore/Untitled design (6).png" >
                 </img> */}
             </Box>
             <Box></Box>
             <Box borderRadius="20px" bgcolor="#8091FF"
-            paddingTop="180px" paddingLeft="80px" color = "#000000">
-
+            paddingTop="60px" paddingLeft="60px" color = "#000000">
+                <Typography fontFamily="prompt" fontWeight="500" variant="h5">Our Top Categories</Typography>
+                <Button sx={{backgroundImage: "url('../images/loginExplore/categories (2).png')", backgroundPositionX: "centre", backgroundPositionY: "centre",
+                backgroundSize: "cover", backgroundRepeat: "no-repeat" ,color: "#000000", height : "80%", width: "18%", borderRadius:"20px", mt: "30px"}}>
+                    <Typography fontFamily="prompt" fontWeight="500" variant="h5">HELLO</Typography>
+                </Button>
+                <Button sx={{backgroundImage: "url('../images/loginExplore/categories (1).png')", backgroundPositionX: "centre", backgroundPositionY: "centre",
+                backgroundSize: "cover", backgroundRepeat: "no-repeat" ,color: "#000000", height : "80%", width: "18%", borderRadius:"20px", mt: "30px", ml: "20px"}}>
+                </Button>
+                <Button sx={{backgroundImage: "url('../images/loginExplore/categories (3).png')", backgroundPositionX: "centre", backgroundPositionY: "centre",
+                backgroundSize: "cover", backgroundRepeat: "no-repeat" ,color: "#000000", height : "80%", width: "18%", borderRadius:"20px", mt: "30px", ml: "20px"}}>
+                </Button>
+                <Button sx={{backgroundImage: "url('../images/loginExplore/categories (4).png')", backgroundPositionX: "centre", backgroundPositionY: "centre",
+                backgroundSize: "cover", backgroundRepeat: "no-repeat" ,color: "#000000", height : "80%", width: "18%", borderRadius:"20px", mt: "30px", ml: "20px"}}>
+                </Button>
+                <Button sx={{backgroundImage: "url('../images/loginExplore/categories (5).png')", backgroundPositionX: "centre", backgroundPositionY: "centre",
+                backgroundSize: "cover", backgroundRepeat: "no-repeat" ,color: "#000000", height : "80%", width: "18%", borderRadius:"20px", mt: "30px", ml: "20px"}}>
+                </Button>
             </Box>
         </Box>
 
