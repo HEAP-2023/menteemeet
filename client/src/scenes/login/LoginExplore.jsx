3df9fdf660a9a47f26680f3306025ce6a0502dcf
alwaystryin88 <srneo.2022@scis.smu.edu.sg>
2023-07-26 21:02:17 +0800

    login pages update

--- client/src/scenes/login/LoginExplore.jsx
+++ client/src/scenes/login/LoginExplore.jsx
@@ -10,7 +10,7 @@ const loginExplore = () => {
     return (
     <Box width="100%" position="relative">
         <LoginOverlay/>
-        <Box display="grid" gridTemplateRows="1fr 4fr 1fr 6fr" gap="20px" width="100%" p="20px">
+        <Box display="grid" gridTemplateRows="1fr 4fr 1fr 6fr 1fr 2fr" gap="20px" width="100%" p="20px">
             <Header></Header>
             <Box sx={{backgroundImage: "url('../images/loginExplore/Untitled design (6).png')"}}>
                 {/* <img src="../images/loginExplore/Untitled design (6).png" >
@@ -21,7 +21,7 @@ const loginExplore = () => {
             paddingTop="60px" paddingLeft="60px" color = "#000000">
                 <Typography fontFamily="prompt" fontWeight="500" variant="h5">Our Top Categories</Typography>
                 <Button sx={{backgroundImage: "url('../images/loginExplore/categories (2).png')", backgroundPositionX: "centre", backgroundPositionY: "centre",
-                backgroundSize: "cover", backgroundRepeat: "no-repeat" ,color: "#000000", height : "80%", width: "18%", borderRadius:"20px", mt: "30px"}}>
+                backgroundSize: "cover", backgroundRepeat: "no-repeat" ,color: "#FFFFFF", height : "80%", width: "18%", borderRadius:"20px", mt: "30px"}}>
                     <Typography fontFamily="prompt" fontWeight="500" variant="h5">HELLO</Typography>
                 </Button>
                 <Button sx={{backgroundImage: "url('../images/loginExplore/categories (1).png')", backgroundPositionX: "centre", backgroundPositionY: "centre",
@@ -37,7 +37,13 @@ const loginExplore = () => {
                 backgroundSize: "cover", backgroundRepeat: "no-repeat" ,color: "#000000", height : "80%", width: "18%", borderRadius:"20px", mt: "30px", ml: "20px"}}>
                 </Button>
             </Box>
+            <Box></Box>
+            <Box borderRadius="20px" bgcolor="#8091FF"
+            paddingTop="60px" paddingLeft="60px" color = "#000000">
+                <Typography fontFamily="prompt" fontWeight="500" variant="h5">All Programmes</Typography>
+            </Box>
         </Box>
+
 
 
     </Box>);
