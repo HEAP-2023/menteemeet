a6acc0744f5d9062746653de179767ce3037e43a
Olivia <oliviaow.2022@scis.smu.edu.sg>
2023-07-27 12:34:14 +0800

    applications tab

--- client/src/components/profile/ProfileNav.jsx
+++ client/src/components/profile/ProfileNav.jsx
@@ -1,14 +1,15 @@
 import { Box, IconButton, Typography, Button } from "@mui/material";
 import { generateColors } from "../../theme";
-
+import { useSelector } from "react-redux";
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 const ProfileNav = () => {
     const colors = generateColors();
+    const acctName = useSelector((state) => state.user.userBasicDetails.name);
     return (<Box width="100%" height="100%" display="flex" justifyContent="center" alignItems="center">
                 <Button variant="contained">
                     <AccountCircleOutlinedIcon color={colors.text[500]} />                    
-                    <Typography variant="h6">username</Typography>
+                    <Typography variant="h6">{acctName}</Typography>
                 </Button>
     </Box>);
 }
