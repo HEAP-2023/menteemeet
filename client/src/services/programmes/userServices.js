09dc963dcbcac29087a523dffe1d5c8a98f56b16
phosphurous <122954378+phosphurous@users.noreply.github.com>
2023-08-04 16:11:03 +0800

    removed Testing

--- client/src/services/programmes/userServices.js
+++ client/src/services/programmes/userServices.js
@@ -23,13 +23,12 @@ export const getSignUpForm = async(id) => {
 }
 
 export const postSignUp = async(signUpForm) => {
-    // const res = await axiosInstance({
-    //     method : "post",
-    //     url : `users/programmes/`,
-    //     headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` },
-    //     data : signUpForm
-    // })
-    const res = console.log("HEY BITCH");
+    const res = await axiosInstance({
+        method : "post",
+        url : `users/programmes/`,
+        headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` },
+        data : signUpForm
+    })
     return res;
 }
 
